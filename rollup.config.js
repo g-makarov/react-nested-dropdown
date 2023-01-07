e314cdf62ec83ae8e06e4752b4f249de7ebe6182
gmakarov <german@appello.com.au>
2023-01-07 21:54:01 +0300

    fix rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -6,10 +6,10 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-ts';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import copy from 'rollup-plugin-copy';
 
-import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 
 /** Custom rollup plugin to exec tsc-alias over the built files */
 function tscAliasPlugin() {
